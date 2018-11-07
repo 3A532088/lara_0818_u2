0f72f1600f99f8f1c3a76d7a11b6cec00e3a4b1c
3A532088 <paileo007007@gmail.com>
2018-11-07 19:38:11 +0800

    傳遞資料至 view 顯示

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/', function () {
     return view('home.index');
 });*/
 Route::get('/home', 'HomeController@index');
+
+Route::get('/hello/{name?}', ['as'=>'hello.index', 'uses'=>'HelloController@index']);
